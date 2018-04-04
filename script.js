0dfcf67401e0fef93e5ca5daeb240dd148f667ef
IvanYakimtsov <ivan.yakimtspv@gmail.com>
2018-04-04 18:27:59 +0300

    classes

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
 window.onload = function () {
-    class Node{
-        getStringValue();
-        calculate();
-    }
-    class OperationNode extends Node{
 
+    class BinaryOperationNode{
+
+        constructor(type) {
+            this.type = type;
+
+            this.childNode1;
+            this.childNode2;
+        }
+
+        getStringValue(){
+            return "("+ this.childNode1.getStringValue() + this.type + this.childNode2.getStringValue() + ")";
+        }
+
+        calculate(){
+            switch (this.type) {
+                case "&":
+                    if(this.childNode1.calculate() === 1 && this.childNode2.calculate() === 1) return 1;
+                    else return 0;
+                case "|":
+                    if(this.childNode1.calculate() === 1 || this.childNode2.calculate() === 1) return 1;
+                    else return 0;
+                case "~":
+                    if((this.childNode1.calculate() === 1 && this.childNode2.calculate() === 1) ||
+                        (this.childNode1.calculate() === 0 && this.childNode2.calculate() === 0)) return 1;
+                    else return 0;
+                case "->":
+                    if(this.childNode1.calculate() === 1 && this.childNode2.calculate() === 0) return 0;
+                    else return 1;
+
+            }
+        }
     }
 
-    class VariableNode extends Node{
-        value;
-        stringValue;
+    class UnaryOperationNode{
+
+        constructor() {
+            this.childNode;
+        }
+
+        getStringValue() {
+            return "(!" + this.childNode.getStringValue() + ")";
+        }
+
+        calculate() {
+            return (this.childNode.calculate() === 0) ? 1 : 0;
+        }
+    }
+
+    class VariableNode{
         constructor(stringValue) {
-            super();
             this.stringValue = stringValue;
         }
 
-        calculate(){
+        calculate() {
             return this.value;
+        }
+
+        getStringValue() {
+            return this.stringValue;
         }
     }
 
-    class ConstNode extends Node{
-        value;
+    class ConstNode{
         constructor(value) {
-            super();
             this.value = value;
         }
 
-        calculate(){
+        calculate() {
+            return this.value;
+        }
+
+        getStringValue() {
             return this.value;
         }
 
     }
 
     document.getElementById('submit').onclick = function () {
-        node = new ConstNode(1);
-        var expression = document.forms["input"].elements["text"].value;
-        createRPN(expression)
+        constNode1 = new ConstNode(1);
+        constNode0 = new ConstNode(0);
+
+        unaryOperationNode = new UnaryOperationNode();
+        unaryOperationNode.childNode = constNode0;
+
+        binaryNode = new BinaryOperationNode("&");
+        binaryNode.childNode1 = constNode1;
+        binaryNode.childNode2 = unaryOperationNode;
+
+        // alert(binaryNode.getStringValue());
+        // alert(binaryNode.calculate());
+
+        var a = [];
+        a.push(constNode1);
+        a.push(unaryOperationNode);
+        a.push(binaryNode);
+        for (var i = 0; i < a.length; i++) {
+            alert(a[i].getStringValue());
+        }
+        // var expression = document.forms["input"].elements["text"].value;
+        //  createRPN(expression)
     }
 
     function createRPN(expression) {
         alert("function " + expression);
         var fixedExp = expression.replace(/->/g, "-");
+
         alert("after " + fixedExp);
     }
-    
+
 
 }
